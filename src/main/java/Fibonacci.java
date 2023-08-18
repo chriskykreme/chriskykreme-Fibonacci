97917af1c1d9e0a77e391c69e5a249dde1d40889
Christopher King <king.cdk@gmail.com>
2023-08-18 15:46:39 +0000

    Added logic to Fibonacci file

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -5,7 +5,8 @@ public class Fibonacci {
      * The fibonacci sequence is calculated by adding the previous two numbers in the sequence to obtain the next
      * number. The first two numbers in the whole fibonacci sequence are always 0 and 1.
      * For instance, the sequence starts as:
-     * 0 1 1 2 3 5 8 13 21
+     * index: 0 1 2 3 4 5 6 7  8 
+     * value: 0 1 1 2 3 5 8 13 21
      * where the 4th number of the sequence would be 3, the 5th number would be 5, the 6th number would be 8, etc.
      *
      * You could either use a for loop to keep track of the current number of the fibonacci sequence as well as the two
@@ -15,6 +16,18 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+        int[] fib;
+        int size = 2 + n;
+        fib = new int[size];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int ctr = 2; ctr < size; ctr++) {
+            fib[ctr] = fib[ctr-2] + fib[ctr-1];
+        }
+        for (int f : fib) {
+            System.out.println(f);
+        }
+        return fib[n];
     }
 }
